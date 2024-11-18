ed6a4863d6b0ba23e7d4a2c7043ece6ba5278526
jecos <jcostanza@ferlab.bio>
2024-11-18 13:10:55 -0500

    fix: import connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { countOccurrences, fetchSeqExp } from './benchmark';
+import connection from './connection';
 import { concurrentUsers } from './env';
 import { takeRandomElemOfArray } from './utils';
 
 const main = async () => {
     console.time('fetchSeqExp');
     const seqExp = await fetchSeqExp();
-
 
     const concurrentUsersSeqExp = [];
 
@@ -16,15 +16,15 @@ const main = async () => {
     console.log('concurrentSeqExp', concurrentUsersSeqExp);
 
     const workerPromises = [];
-    for (const seqExp of concurrentUsersSeqExp){
-         workerPromises.push( countOccurrences(seqExp.seq_id, seqExp.part));
+    for (const seqExp of concurrentUsersSeqExp) {
+        workerPromises.push(countOccurrences(seqExp.seq_id, seqExp.part));
         // workerPromises.push( listOccurrences(seqExp.seq_id, seqExp.part).catch((err) => {
         //     console.error(err);
         // }));
     }
     await Promise.all(workerPromises);
     console.timeEnd('fetchSeqExp');
-    await connection.getInstance().end()
+    await connection.getInstance().end();
 };
 
 main()
